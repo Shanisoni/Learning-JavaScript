0403c73c67d04e5bba0a71d7347fc6c57666ba76
Shani Soni <shanisoni7084@gmail.com>
2024-04-10 23:01:39 +0530

    Commited

---
+++ Conditions/Conditions.js
@@ -0,0 +1,17 @@
+
+
+
+//  =  single equal means to assignnment 
+
+//  == to check or compare 
+
+//  === Triple Equal refers to strict check 
+
+//  It means it checks for the value and type both 
+
+
+// Implicit Scope is that when we use if(xy) without any curly brackets means open scope 
+
+const shani = 900 ;
+
+if( shani > 800)  console.log(shani) ; 
