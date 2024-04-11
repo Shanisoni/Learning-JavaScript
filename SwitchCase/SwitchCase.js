03b2fdf25660b057002c33a868870ab80595e09c
Shani Soni <shanisoni7084@gmail.com>
2024-04-11 06:42:25 +0530

    Commited

---
+++ SwitchCase/SwitchCase.js
@@ -0,0 +1,40 @@
+
+const  month = 4 ;
+
+
+switch(month) {
+    case 1 : 
+    console.log("Janauray")
+    break;
+
+    case 2:
+    console.log("Feburary")
+    break ;
+
+    case 3 :
+        console.log("March")
+        break;
+
+    case 4 : 
+    console.log("April")
+    break;
+
+    default :
+    console.log("default")
+
+
+}
+
+
+// Falsy Values 
+
+
+// false    , 0 , -0 , BigInt 0n , "" , null , undefined , NaN 
+
+
+// Truthy Value 
+
+
+//  Anything in the string is truthy value 
+
+// " 0 " , 'false' , "-0" ,  " "  , [ ] , { } , function() {}
