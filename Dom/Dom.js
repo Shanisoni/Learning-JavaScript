f1c85a07ed6c622690649437652d8b66f3e1cd65
Shani Soni <shanisoni7084@gmail.com>
2024-04-13 13:35:30 +0530

    Commited

---
+++ Dom/Dom.js
@@ -0,0 +1,15 @@
+// document.getElementById('title') 
+// By this method we can get the reference of specified element 
+
+
+// document.getElementById('title').getAttribute('id') 
+
+//  In this by using getAttribute method we can get the type of attribut which is title
+
+
+// To extract attributes from the console we use getElementById method 
+
+// Similarly to overwrite the values we  use setAttribute method 
+
+
+//  
