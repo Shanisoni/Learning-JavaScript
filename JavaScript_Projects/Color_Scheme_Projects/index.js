731c4237994065f8a4d20c85cd701c67e4f23ab1
Shani Soni <shanisoni7084@gmail.com>
2024-04-14 16:55:06 +0530

    ommited

---
+++ JavaScript_Projects/Color_Scheme_Projects/index.js
@@ -0,0 +1,40 @@
+// const buttons  = document.querySelectorAll('.button');
+// const body = document.querySelector('body');
+
+// buttons.forEach( function(button) {
+//     console.log(buttons);
+
+//     button.addEventListener('click' , function (e) {
+//       console.log(e);
+//       console.log(e.target);
+
+//       if(e.target.id === 'grey') {
+//         body.style.backgroundColor = e.target.id;
+//       }
+
+//       if(e.target.id==='yellow') {
+//         body.style.backgroundColor = e.target.id;
+//       }
+
+//       if(e.target.id==='blue') {
+//         body.style.backgroundColor = e.target.id;
+//       }
+
+//       if(e.target.id==='white') {
+//         body.style.backgroundColor = e.target.id;
+//       }
+//     });
+
+// });
+
+const button = document.querySelectorAll('.button');
+
+const body = document.querySelector('body');
+
+button.forEach( function(button)  {
+    console.log(button);
+
+     
+    
+} )
+
