8ed7ce8d2309ed0cf7a2c37efc976df0b66fe962
Shani Soni <shanisoni7084@gmail.com>
2024-04-17 12:32:51 +0530

    Commited

--- JavaScript_Projects/Number_Guessing_Game/number.js
+++ JavaScript_Projects/Number_Guessing_Game/number.js
@@ -5,3 +5,59 @@ const randomNumber = parseInt(Math.random() * 100 + 1)
  const userInput = document.querySelector('#guessField');
  const guessesSlot = document.querySelector('.guesses');
  const remaining = document.querySelector('.lastResult');
+ const lowOrHi = document.querySelector('.lowOrHi');
+ const startOver = document.querySelector('.resultParas');
+
+
+ const p = document.createElement('p')
+
+//  This array is made for the user to store his entered values so that he can avoid entering same numbers 
+
+ let prevGuess = [] ;  
+
+
+//  to count number of guesses / num of attempts 
+
+let numGuess  = 1 ;
+
+// Now we take a variable that starts the game or ends it 
+
+let playGame = true ;
+
+// To validate the input of ther user  we make function
+
+function validateGuess(guess) {
+
+}
+
+// To Check the input values and print them 
+
+function checkGuess(guess) {
+
+
+
+}
+
+
+function displayGuess(guess) {
+
+}
+
+// To perform the DOM Manipulations we use display methhod 
+// Basicaly we take inputs from the user and display that 
+
+function displayMessage(message) {
+
+}
+
+
+function endGame() {
+
+}
+
+
+
+
+function newGame() {
+
+}
